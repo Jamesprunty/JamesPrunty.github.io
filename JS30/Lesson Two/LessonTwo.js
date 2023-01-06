7d210196b9ae6d8e147fb874a965c100dffe14f9
Jamesprunty <james107@hotmail.com>
2023-01-06 13:07:19 +0000

    Working Clock

--- JS30/Lesson Two/LessonTwo.js
+++ JS30/Lesson Two/LessonTwo.js
@@ -1,6 +1,8 @@
 
-
+//get the second hand
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 
 
@@ -9,8 +11,18 @@ function setDate() {
     const now = new Date();
     // Make a const for seconds and set to "getSeconds()"
     const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
     // Get the percentage by deviding by 60, then times by 360 to get the degrees
-    const secondsDegrees = ((seconds / 60) * 360);
+    //We add 90 degress to offset the default 90 we put in.
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    //Set the secondhand rotate to the seconds degrees
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
     console.log(seconds);
 }
 
