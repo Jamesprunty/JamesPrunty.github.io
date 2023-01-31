14b27b1bd7dbb240a33bd01fcb36ca3d536ca8ed
Jamesprunty <james107@hotmail.com>
2023-01-31 15:11:05 +0000

    Start Lesson 10

--- JS30/Lesson 10/Lesson-10.js
+++ JS30/Lesson 10/Lesson-10.js
@@ -1,1 +1,24 @@
-const inbox = document.querySelectorAll('.inbox')
+const inbox = document.querySelectorAll('.item')
+
+console.log(inbox);
+
+inbox.forEach(element => {
+
+    addEventListener('change', function(){
+
+        console.log(this);
+
+        if(this.checked){
+
+            console.log("Checkbox Checked");
+
+        }else{
+
+            console.log("Checkbox unchecked");
+
+        }
+        
+
+    })
+    
+});
