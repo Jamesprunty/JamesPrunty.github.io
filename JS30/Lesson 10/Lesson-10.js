d137ec68268002419ec4aaf8343b93690ef30bb6
Jamesprunty <james107@hotmail.com>
2023-02-01 13:35:48 +0000

    testing checkboxes

--- JS30/Lesson 10/Lesson-10.js
+++ JS30/Lesson 10/Lesson-10.js
@@ -1,14 +1,101 @@
-const inbox = document.querySelectorAll('.item')
+const inbox = document.querySelectorAll('input[type="checkbox"]')
+let shiftDown = false;
+let pressed = "0";
+let lastchecked;
+let newchecked;
+
 
 console.log(inbox);
 
 inbox.forEach(element => {
 
-    addEventListener('change', handleChange);    
+    addEventListener('change', handleChange);
+
 });
 
-function handleChange () {
 
-console.log(this.name);
+function handleChange() {
+
+    console.log(this.input);
+    console.log(inbox[0]);
+    
+
+
+    if (shiftDown) {
+
+
+
+
+
+
+
+
+    } else {
+
+
+        return;
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 
 }
+
+window.addEventListener('keydown', function (e) {
+
+    if (pressed == "0") {
+
+        if (e.key == "Shift") {
+
+            shiftDown = true;
+            console.log("ShiftDown");
+
+            pressed++;
+        }
+    }
+});
+
+window.addEventListener('keyup', function (e) {
+
+    if (e.key == "Shift") {
+
+        shiftDown = false;
+        console.log("ShiftUp");
+
+        pressed = "0";
+    }
+});
+
+
+
+
+
+//Get whether the shift button is down - DONE
+
+//Get which box is checked
+
+//Get which box is being checked
+
+//Find all the boxes in between
+
+//Check those boxes
+
+
+
+
+
+
+
+
+
