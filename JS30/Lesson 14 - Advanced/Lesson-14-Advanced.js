2422f5cf157532e5963957f9dcf688368cdc6b88
Jamesprunty <james107@hotmail.com>
2023-02-10 00:28:24 +0000

    First set of data funcs

--- JS30/Lesson 14 - Advanced/Lesson-14-Advanced.js
+++ JS30/Lesson 14 - Advanced/Lesson-14-Advanced.js
@@ -1,55 +1,182 @@
 let table = document.getElementById('container');
 
-let buttons = document.querySelectorAll(".controlOption");
-
-let referenceBtn = document.querySelector("#reference");
-let spliceBtn = document.querySelector("#splice");
-let concatBtn = document.querySelector("#concat");
-let spreadBtn = document.querySelector("#spread");
-let assignBtn = document.querySelector("#assign");
-let resetBtn = document.querySelector("#reset");
-
-
-
-
+let buttons = document.querySelectorAll('input[type="button"]');
 
 let tableMade = false;
+let table2Made = false;
 rowCount = 0;
 const data = [{
 
-  name: "James" ,
+  name: "James",
   lastName: "Prunty",
   age: "28"
 
 }];
 
+const dataArray = ["SJames", "Prunty", "28"]
+
+
+
+
 
 document.querySelector("#originalTable").insertAdjacentHTML('afterend',
+  `<table id="table"><thead><tr><th>
+     ${Object.keys(data[0]).join('<th>')}
+    </thead><tbody><tr><td>${data.map(e => Object.values(e)
+    .join('<td>')).join('<tr><td>')}</table>`)
+
+
+
+tableMade = true;
+rowCount++
+
+
+function processData(e) {
+
+  let func = this.value;
+
+
+  if (tableMade) {
+
+
+    document.getElementById("table").remove();
+  }
+
+  if (table2Made) {
+
+    document.getElementById("table2").remove();
+
+  }
+
+  switch (func) {
+
+
+    case "reference":
+      
+     const newData = data;
+      newData[0].name = "Jeremy";
+      console.log(newData);
+
+      document.querySelector("#originalTable").insertAdjacentHTML('afterend',
         `<table id="table"><thead><tr><th>
      ${Object.keys(data[0]).join('<th>')}
     </thead><tbody><tr><td>${data.map(e => Object.values(e)
-            .join('<td>')).join('<tr><td>')}</table>`)
+          .join('<td>')).join('<tr><td>')}</table>`)
 
-    tableMade = true;
-    rowCount++
+      document.querySelector("#newTable").insertAdjacentHTML('afterend',
+        `<table id="table2"><thead><tr><th>
+       ${Object.keys(newData[0]).join('<th>')}
+      </thead><tbody><tr><td>${newData.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
 
+      table2Made = true;
 
-    function processData (e){
-
-        console.log(e);
-        console.log(this);
-
-
-        console.log(this);
+      break;
 
 
+    case "slice":
 
-    }
+      const newData2 = data.slice();
+      newData2[0].name = "Jeremy";
+      console.log(newData2);
+      console.log(data);
 
-    buttons.forEach(element => {
-        
-        element.addEventListener('click', processData);
+      document.querySelector("#originalTable").insertAdjacentHTML('afterend',
+        `<table id="table"><thead><tr><th>
+       ${Object.keys(data[0]).join('<th>')}
+      </thead><tbody><tr><td>${data.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
 
-    });
+      document.querySelector("#newTable").insertAdjacentHTML('afterend',
+        `<table id="table2"><thead><tr><th>
+         ${Object.keys(newData2[0]).join('<th>')}
+        </thead><tbody><tr><td>${newData2.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
 
-   
+      table2Made = true;
+
+
+      break;
+
+    case "concat":
+      const newData3 = [].concat(data);
+      newData3[0].name = "Jack";
+      console.log(newData3);
+      console.log(data);
+
+      document.querySelector("#originalTable").insertAdjacentHTML('afterend',
+        `<table id="table"><thead><tr><th>
+       ${Object.keys(data[0]).join('<th>')}
+      </thead><tbody><tr><td>${data.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
+
+      document.querySelector("#newTable").insertAdjacentHTML('afterend',
+        `<table id="table2"><thead><tr><th>
+         ${Object.keys(newData3[0]).join('<th>')}
+        </thead><tbody><tr><td>${newData3.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
+
+      table2Made = true;
+
+      break;
+
+    case "spread":
+
+      const newData4 = {...data};
+      newData4[0].name = "Jamie";
+      console.log(newData4);
+      console.log(data);
+
+      document.querySelector("#originalTable").insertAdjacentHTML('afterend',
+        `<table id="table"><thead><tr><th>
+       ${Object.keys(data[0]).join('<th>')}
+      </thead><tbody><tr><td>${data.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
+
+      document.querySelector("#newTable").insertAdjacentHTML('afterend',
+        `<table id="table2"><thead><tr><th>
+         ${Object.keys(newData4[0]).join('<th>')}
+        </thead><tbody><tr><td>${newData4.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
+      break;
+
+    case "assign":
+      newData5 = Object.assign({}, data, {name:"Jonah"});
+
+      console.log(newData5);
+      console.log(data);
+
+      document.querySelector("#originalTable").insertAdjacentHTML('afterend',
+        `<table id="table"><thead><tr><th>
+       ${Object.keys(data[0]).join('<th>')}
+      </thead><tbody><tr><td>${data.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
+
+      document.querySelector("#newTable").insertAdjacentHTML('afterend',
+        `<table id="table2"><thead><tr><th>
+         ${Object.keys(newData5[0]).join('<th>')}
+        </thead><tbody><tr><td>${newData5.map(e => Object.values(e)
+          .join('<td>')).join('<tr><td>')}</table>`)
+      break;
+      break;
+
+    case "refresh":
+      location.reload();
+      console.log(this.value);
+      break;
+
+    default:
+      console.log(this.value);
+
+  }
+
+
+
+}
+
+buttons.forEach(element => {
+
+  element.addEventListener('click', processData);
+
+});
+
