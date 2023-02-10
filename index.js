7a11b0f241e6c679dc291ab5ee906ce07b310ffc
Jamesprunty <james107@hotmail.com>
2023-02-10 22:50:31 +0000

    First Animation

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-
-
-
-
-
 const title = document.querySelector(".titleContainer");
+const menuContainer = document.querySelector("#MenuContainer");
+const SHDiv = document.querySelector("#SHDiv");
+const page = document.querySelector("#page");
+
 title.addEventListener('click', openMenu);
 
 
 
 function openMenu(){
+
+    title.classList.add("titleAnimate");
+    menuContainer.classList.remove("hidden");
+    SHDiv.classList.add("subtitleAnimate");
+    page.classList.add("backgroundAnimate");
+   
+
 
 console.log("Menu Open");
 
