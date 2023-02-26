e14fd586a3f9367c3299283aec54bc0a8b9d721a
Jamesprunty <james107@hotmail.com>
2023-02-26 12:32:06 +0000

    check working

--- JS30/Lesson-15/Lesson-15.js
+++ JS30/Lesson-15/Lesson-15.js
@@ -27,19 +27,22 @@ function addItem(e){
 }
 
 //Add this in as an empty array because if you forget to put anything in it won't fail. As it will just loop over nothing.
+//Every time we run this it recreates the whole list
 function populateList(items = [], itemsList){
 
     //This goes through the items in the array and puts them as HTML in "platesList"
 itemsList.innerHTML = items.map((item, i) => {
 
+    //Link the label to the input by making the "id" and the "For" the same
+    //We will add a ternerary operator. If "item.done" is true, then check the box, otherwise do nothing. 
+
     return `
     
     <li>
-        <label for="">${item.text}</label>
+        <input type="checkbox" data-index=${i} id="item${i}" ${item.done ? 'checked' : ''}/>
+        <label for="item${i}">${item.text}</label>
     </li>
-    
-    
-    
+
     `;
 
 //We have to join it because usally this will give us an array, when we want one big string
