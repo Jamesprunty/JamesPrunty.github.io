854fd8587b339d7d94af5dd97547d98095446a6e
Jamesprunty <james107@hotmail.com>
2023-03-05 10:09:58 +0000

    hover working

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let circles = document.querySelectorAll("circle");
 let r = document.querySelector(':root');
 let SVGContainer = document.querySelector("#SVGContainer");
 r.style.setProperty('--running', "paused");
+let menuCircle = document.querySelector(".menuCircle")
+let topMenuBG = document.querySelector(".topMenuBG");
+let bottomMenuBG = document.querySelector(".bottomMenuBG");
 
 console.log(circles);
 
@@ -32,8 +35,55 @@ console.log("TEST");
 
 if(state == "on"){
     r.style.setProperty('--running', "running");
+    menuCircle.classList.add("scaleUp");
+    topMenuBG.classList.add("BGup");
+    bottomMenuBG.classList.add("BGdown");
+    if(menuCircle.classList.contains("scaleDown")){
+        menuCircle.classList.remove("scaleDown"); 
+    }
+    if(topMenuBG.classList.contains("BGupR")){
+        topMenuBG.classList.remove("BGupR"); 
+    }
+    if(bottomMenuBG.classList.contains("BGdownR")){
+        bottomMenuBG.classList.remove("BGdownR"); 
+    }
+    /*
+    menuHover.forEach(element => {
+        element.classList.add("scaleUp");
+
+        if(element.classList.contains("scaleDown")){
+            element.classList.remove("scaleDown"); 
+        }
+        
+    });
+*/
+
 }else if(state === "off"){
     r.style.setProperty('--running', "paused");
+
+    menuCircle.classList.add("scaleDown");
+    topMenuBG.classList.add("BGupR");
+    bottomMenuBG.classList.add("BGdownR");
+    if(menuCircle.classList.contains("scaleUp")){
+        menuCircle.classList.remove("scaleUp"); 
+    }
+    if(topMenuBG.classList.contains("BGup")){
+        topMenuBG.classList.remove("BGup"); 
+    }
+    if(bottomMenuBG.classList.contains("BGdown")){
+        bottomMenuBG.classList.remove("BGdown"); 
+    }
+    
+
+    /*
+    menuHover.forEach(element => {
+        element.classList.add("scaleDown");
+        if(element.classList.contains("scaleUp")){
+            element.classList.remove("scaleUp"); 
+        }
+    });
+    */
+
 }
 
 
