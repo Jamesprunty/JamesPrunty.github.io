c8ca7c183e25f1f755b28cef60e9fdd1ddae2e67
Jamesprunty <james107@hotmail.com>
2023-03-24 20:52:13 +0000

    test

--- JS30/Lesson-26/Lesson-26.js
+++ JS30/Lesson-26/Lesson-26.js
@@ -1,3 +1,5 @@
 const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
+
+console.log("TEST");
