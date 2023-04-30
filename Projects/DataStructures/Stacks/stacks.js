5b35c3d873673bfcb7cf0317d0f6de3c40cd2acb
Jamesprunty <james107@hotmail.com>
2023-04-30 15:17:01 +0100

    Stack

--- Projects/DataStructures/Stacks/stacks.js
+++ Projects/DataStructures/Stacks/stacks.js
@@ -1,21 +1,60 @@
 /* Stacks! */
 
+
+let push = document.querySelector("#push");
+let newElem = document.querySelector("#newElem");
+let pop = document.querySelector("#pop");
+let peek = document.querySelector("#peek");
+let length = document.querySelector("#length");
+let stack = document.querySelector("#stack");
+
+let buttons = document.querySelectorAll(".btn");
+buttons.forEach(element => {
+    element.addEventListener('click', function(){
+
+        console.log(this.id);
+
+        if(this.id == "push"){
+
+           
+
+        }else if(this.id == "pop"){
+
+           
+
+        }else if (this.id == "peek"){
+
+            
+
+        }else if(this.id == "length"){
+
+           
+
+        }
+
+
+
+    })
+});
+
+
+
 // functions: push, pop, peek, length
 
 var letters = []; // this is our stack
 
 var word = "freeCodeCamp"
 
-var rword = "";
+var rword = "";//This is the reversed word to check
 
 // put letters of word into stack
 for (var i = 0; i < word.length; i++) {
-   letters.push(word[i]);
+   letters.push(word[i]); //Create an array with each letter as a separate string.
 }
 
 // pop off the stack in reverse order
 for (var i = 0; i < word.length; i++) {
-   rword += letters.pop(); 
+   rword += letters.pop(); //This will pop each letter off in the reverse order
 }
 
 if (rword === word) {
@@ -29,34 +68,34 @@ else {
 
 // Creates a stack
 var Stack = function() {
-    this.count = 0;
-    this.storage = {};
+    this.count = 0; //Starts the stack with 0
+    this.storage = {}; //Creates an empty array
   
     // Adds a value onto the end of the stack
-    this.push = function(value) {
-        this.storage[this.count] = value;
-        this.count++;
+    this.push = function(value) {//This is the push function and requires a value
+        this.storage[this.count] = value;//Add to the storage, the value at the count (which is the total amount)
+        this.count++; //Add one to the count so it keeps up. 
     }
     
     // Removes and returns the value at the end of the stack
     this.pop = function() {
-        if (this.count === 0) {
-            return undefined;
+        if (this.count === 0) { //If there is nothing in the stack
+            return undefined; //Return 0
         }
 
-        this.count--;
-        var result = this.storage[this.count];
-        delete this.storage[this.count];
-        return result;
+        this.count--; //Take one off the count
+        var result = this.storage[this.count];//Then retun the storage at the count amount
+        delete this.storage[this.count]; //Then delete it from the stack
+        return result; 
     }
     
     this.size = function() {
-        return this.count;
+        return this.count; //Return the count
     }
     
     // Returns the value at the end of the stack
     this.peek = function() {
-        return this.storage[this.count-1];
+        return this.storage[this.count-1]; //Returns the last item in the stack without deleting it
     }
 }
 
