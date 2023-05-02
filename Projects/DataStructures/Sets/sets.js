73c36237201a1ac214ffb8a9202502d334b98c7f
Jamesprunty <121761095+Jamesprunty@users.noreply.github.com>
2023-05-02 13:39:50 +0100

    Sets

--- Projects/DataStructures/Sets/sets.js
+++ Projects/DataStructures/Sets/sets.js
@@ -5,21 +5,21 @@ function mySet() {
     var collection = [];
     // this method will check for the presence of an element and return true or false
 
-    this.has = function(element) {
+    this.has = function (element) {
         return (collection.indexOf(element) !== -1);
 
     };
 
 
     // this method will return all the values in the set
-    this.values = function() {
+    this.values = function () {
         return collection;
     };
 
 
     // this method will add an element to the set
-    this.add = function(element) {
-        if(!this.has(element)){
+    this.add = function (element) {
+        if (!this.has(element)) {
             collection.push(element);
             return true;
         }
@@ -28,10 +28,10 @@ function mySet() {
 
 
     // this method will remove an element from a set
-    this.remove = function(element) {
-        if(this.has(element)){
+    this.remove = function (element) {
+        if (this.has(element)) {
             index = collection.indexOf(element);
-            collection.splice(index,1);
+            collection.splice(index, 1);
             return true;
         }
         return false;
@@ -40,21 +40,21 @@ function mySet() {
 
 
     // this method will return the size of the collection
-    this.size = function() {
+    this.size = function () {
         return collection.length;
     };
 
 
 
     // this method will return the union of two sets
-    this.union = function(otherSet) {
+    this.union = function (otherSet) {
         var unionSet = new mySet();
         var firstSet = this.values();
         var secondSet = otherSet.values();
-        firstSet.forEach(function(e){
+        firstSet.forEach(function (e) {
             unionSet.add(e);
         });
-        secondSet.forEach(function(e){
+        secondSet.forEach(function (e) {
             unionSet.add(e);
         });
         return unionSet;
@@ -63,11 +63,11 @@ function mySet() {
 
 
     // this method will return the intersection of two sets as a new set
-    this.intersection = function(otherSet) {
+    this.intersection = function (otherSet) {
         var intersectionSet = new mySet();
         var firstSet = this.values();
-        firstSet.forEach(function(e){
-            if(otherSet.has(e)){
+        firstSet.forEach(function (e) {
+            if (otherSet.has(e)) {
                 intersectionSet.add(e);
             }
         });
@@ -77,11 +77,11 @@ function mySet() {
 
 
     // this method will return the difference of two sets as a new set
-    this.difference = function(otherSet) {
+    this.difference = function (otherSet) {
         var differenceSet = new mySet();
         var firstSet = this.values();
-        firstSet.forEach(function(e){
-            if(!otherSet.has(e)){
+        firstSet.forEach(function (e) {
+            if (!otherSet.has(e)) {
                 differenceSet.add(e);
             }
         });
@@ -91,24 +91,25 @@ function mySet() {
 
 
     // this method will test if the set is a subset of a different set
-    this.subset = function(otherSet) {
+    this.subset = function (otherSet) {
         var firstSet = this.values();
-        return firstSet.every(function(value) {
-          return otherSet.has(value);
+        return firstSet.every(function (value) {
+            return otherSet.has(value);
         });
     };
 }
 
 
 let set1 = new mySet();
-set1.add("a");
-set1.add("b");
 set1.add("c");
+set1.add("b");
+set1.add("a");
 set1.add("d");
 set1.add("e");
 set1.add("f");
 
 let set2 = new mySet();
+set2.add("f");
 
 let add = document.querySelector("#add");
 let has = document.querySelector("#has");
@@ -120,41 +121,120 @@ let intersection = document.querySelector("#intersection");
 let difference = document.querySelector("#difference");
 let subset = document.querySelector("#subset");
 
+let newElem = document.querySelector("#newElem");
+let checkElem = document.querySelector("#checkElem");
+let removeElem = document.querySelector("#removeElem");
 
-function updateSet(){
+let output = document.querySelector("#outputText");
 
 
+
+let set1Div = document.querySelector("#set1");
+let set2Div = document.querySelector("#set2");
+
+dataFinishSet1 = "";
+dataFinishSet2 = "";
+
+let buttons = document.querySelectorAll(".btn");
+buttons.forEach(element => {
+
+    element.addEventListener('click', function () {
+
+
+
+        if (this.id == "add") {
+
+            console.log(newElem.value);
+            set2.add(newElem.value);
+            updateSet();
+
+        } else if (this.id == "has") {
+
+            outputText
+
+        } else if (this.id == "values") {
+
+        } else if (this.id == "remove") {
+
+        } else if (this.id == "size") {
+
+        } else if (this.id == "union") {
+
+        } else if (this.id == "intersection") {
+
+        } else if (this.id == "difference") {
+
+        } else if (this.id == "subset") {
+
+        }
+
+
+    });
+})
+
+
+function updateSet() {
+
+
+
+    dataFinishSet1 = "";
+    dataFinishSet2 = "";
+
+
+    for (i = 0; i < set1.size(); i++) {
+
+        let data = `<div class="setItem"><p>${set1.values()[i]}</p></div>`;
+
+        dataFinishSet1 += data;
+
+
+
+    }
+
+    for (i = 0; i < set2.size(); i++) {
+
+        let data = `<div class="setItem"><p>${set2.values()[i]}</p></div>`;
+
+        dataFinishSet2 += data;
+
+
+
+    }
+
+    let dataComplete = JSON.stringify(dataFinishSet1);
+
+
+    console.log(dataComplete);
+
+
+
+    set1Div.innerHTML = dataFinishSet1;
+    set2Div.innerHTML = dataFinishSet2;
 
 }
 
+updateSet();
 
 
 
-
-
-
-
-
-
-
-var setA = new mySet();  
-var setB = new mySet(); 
-setA.add("a");  
-setB.add("b");  
-setB.add("c");  
-setB.add("a");  
-setB.add("d");  
+var setA = new mySet();
+var setB = new mySet();
+setA.add("c");
+setB.add("b");
+setB.add("a");
+setB.add("d");
+setB.add("e");
 console.log(setA.subset(setB));
 console.log(setA.intersection(setB).values());
 console.log(setB.difference(setA).values());
 
-var setC = new Set();  
-var setD = new Set();  
-setC.add("a");  
-setD.add("b");  
-setD.add("c");  
-setD.add("a");  
-setD.add("d");  
+var setC = new Set();
+var setD = new Set();
+setC.add("a");
+setD.add("b");
+setD.add("c");
+setD.add("a");
+setD.add("d");
 console.log(setD.values())
 setD.delete("a");
 console.log(setD.has("a"));
