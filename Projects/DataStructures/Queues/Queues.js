3f1fe573bdd46b4b29e1609884ebd182a73a2861
Jamesprunty <james107@hotmail.com>
2023-05-03 14:08:27 +0100

    queues

--- Projects/DataStructures/Queues/Queues.js
+++ Projects/DataStructures/Queues/Queues.js
@@ -1,32 +1,32 @@
 /* Queues */
 
-function Queue () { 
+function Queue() {
     collection = [];
-    this.print = function() {
+    this.print = function () {
         console.log(collection);
     };
-    this.enqueue = function(element) {
+    this.enqueue = function (element) {
         collection.push(element);
     };
-    this.dequeue = function() {
-        return collection.shift(); 
+    this.dequeue = function () {
+        return collection.shift();
     };
-    this.front = function() {
+    this.front = function () {
         return collection[0];
     };
-    this.size = function() {
-        return collection.length; 
+    this.size = function () {
+        return collection.length;
     };
-    this.isEmpty = function() {
-        return (collection.length === 0); 
+    this.isEmpty = function () {
+        return (collection.length === 0);
     };
-    this.get = function(i) {
+    this.get = function (i) {
         return collection[i];
     }
 }
 
-var q = new Queue(); 
-q.enqueue('a'); 
+var q = new Queue();
+q.enqueue('a');
 q.enqueue('b');
 q.enqueue('c');
 q.print();
@@ -36,46 +36,56 @@ q.print();
 
 
 
-function PriorityQueue () {
+function PriorityQueue() {
     var collection = [];
-    this.printCollection = function() {
-      (console.log(collection));
+    this.printCollection = function () {
+        (console.log(collection));
     };
-    this.enqueue = function(element){
-        if (this.isEmpty()){ 
+    this.enqueue = function (element) {
+        if (this.isEmpty()) {
             collection.push(element);
         } else {
             var added = false;
-            for (var i=0; i<collection.length; i++){
-                 if (element[1] < collection[i][1]){ //checking priorities
-                    collection.splice(i,0,element);
+            for (var i = 0; i < collection.length; i++) {
+                if (element[1] < collection[i][1]) { //checking priorities
+                    collection.splice(i, 0, element);
                     added = true;
                     break;
                 }
             }
-            if (!added){
+            if (!added) {
                 collection.push(element);
             }
         }
     };
-    this.dequeue = function() {
+    this.dequeue = function () {
         var value = collection.shift();
         return value[0];
     };
-    this.front = function() {
+    this.front = function () {
         return collection[0];
     };
-    this.size = function() {
-        return collection.length; 
+    this.size = function () {
+        return collection.length;
     };
-    this.isEmpty = function() {
-        return (collection.length === 0); 
+    this.isEmpty = function () {
+        return (collection.length === 0);
     };
+    this.get = function (i) {
+        return collection[i];
+    }
 }
 
 
 
-var pq = new PriorityQueue(); 
+var pq = new PriorityQueue();
+
+pq.enqueue(['Ace', 1]);
+pq.enqueue(['King', 2]);
+pq.enqueue(['Queen', 2]);
+pq.enqueue(['Jack', 3]);
+pq.enqueue(['10', 4]);
+
 
 let enqueue = document.querySelector("#enqueue");
 let enqueueP = document.querySelector("#enqueueP");
@@ -91,6 +101,9 @@ let print = document.querySelector("#print");
 let printP = document.querySelector("#printP");
 let queueDiv = document.querySelector("#queue");
 let queueDivP = document.querySelector("#queueP");
+let newElem = document.querySelector("#newElem");
+let newElemP = document.querySelector("#newElemP");
+let newPriorP = document.querySelector("#newPriorP");
 
 let buttons = document.querySelectorAll(".btn");
 
@@ -98,81 +111,113 @@ let dataFinish1 = "";
 let dataFinish2 = "";
 
 buttons.forEach(element => {
-    element.addEventListener('click', function(){
+    element.addEventListener('click', function () {
 
 
-        if(this.id == "enqueue"){
-
-          
-        }else if(this.id == "enqueueP"){
+        if (this.id == "enqueue") {
 
 
-        }else if(this.id == "dequeue"){
+            q.enqueue(newElem.value);
+            updateQueues();
+
+
+        } else if (this.id == "enqueueP") {
+
+            console.log(newPriorP.value);
+
+            if(newPriorP.value > 0 && newElemP.value != 0){
+              
+
+                let input = [newElemP.value, newPriorP.value];
+                pq.enqueue(input);
+
+            }else{
+
+                outputTextP.innerText = "Please enter a priority & value";
+                return;
+             
+            }
 
             
+            updateQueues();
+
+        } else if (this.id == "dequeue") {
+
+            q.dequeue();
+            updateQueues();
+
         }
-        else if(this.id == "dequeueP"){
+        else if (this.id == "dequeueP") {
 
-            
+            pq.dequeue();
+            updateQueues();
+
         }
-        else if(this.id == "front"){
+        else if (this.id == "front") {
 
-            
+            outputText.innerText = q.front();
+
         }
-        else if(this.id == "frontP"){
+        else if (this.id == "frontP") {
 
-            
+            outputTextP.innerText = pq.front();
+
         }
-        else if(this.id == "size"){
+        else if (this.id == "size") {
 
-            
+            outputText.innerText = q.size();
+
         }
-        else if(this.id == "sizeP"){
+        else if (this.id == "sizeP") {
 
-            
+            outputTextP.innerText = pq.size();
+
         }
-        else if(this.id == "empty"){
+        else if (this.id == "empty") {
 
-            
+            outputText.innerText = q.isEmpty();
+
         }
-        else if(this.id == "emptyP"){
+        else if (this.id == "emptyP") {
 
-            
-        }
-        else if(this.id == "print"){
+            outputTextP.innerText = pq.isEmpty();
 
-            
-        }else if(this.id == "printP"){
-
-            
         }
 
 
     })
-    
+
 });
 
-function updateQueues(){
+function updateQueues() {
+
+    dataFinish1 = "";
+    dataFinish2 = "";
 
 
-    for(i=0;i<q.size();i++){
+    for (i = 0; i < q.size(); i++) {
+
+
 
         let data = `<div class="queueItem"><p>${q.get(i)}</p></div>`;
-       
-        dataFinish1 += data;
 
-        
+        dataFinish1 += data;
 
     }
 
-    //let dataComplete = JSON.stringify(dataFinish1);
+    for (i = 0; i < pq.size(); i++) {
 
-    
+        let data2 = `<div class="queueItem"><p>${pq.get(i)}</p></div>`;
 
-    console.log(dataFinish1);
+        dataFinish2 += data2;
+
+    }
+
+    console.log(dataFinish2);
 
     queue.innerHTML = dataFinish1;
-    
+    queueP.innerHTML = dataFinish2;
+
 
 
 }
@@ -180,11 +225,4 @@ function updateQueues(){
 updateQueues();
 
 
-pq.enqueue(['Beau Carnes', 2]); 
-pq.enqueue(['Quincy Larson', 3]);
-pq.enqueue(['Ewa Mitulska-Wójcik', 1])
-pq.enqueue(['Briana Swift', 2])
-pq.printCollection();
-pq.dequeue();
-console.log(pq.front());
-pq.printCollection();
+
