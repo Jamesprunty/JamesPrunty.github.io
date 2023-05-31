cce98f59edca3f85ab0e8e5adb195b6dc04fbf1c
Jamesprunty <james107@hotmail.com>
2023-05-31 15:31:31 +0100

    Heap

--- Projects/Datastructure/Heap/Heap.js
+++ Projects/Datastructure/Heap/Heap.js
@@ -4,9 +4,29 @@
 // right child: i * 2 + 1
 // parent: i / 2
 
+let minChk = document.querySelector("#minCheck");
+let maxChk = document.querySelector("#maxCheck");
+
+minChk.addEventListener('click', function(){
+	console.log("minCheck");
+	maxChk.checked = false;
+});
+maxChk.addEventListener('click', function(){
+	console.log("maxCheck");
+	minChk.checked = false;
+});
+
+minChk.checked = true;
+
+let svg = document.querySelector("#heapSVG");
+let svgSize = svg.getBoundingClientRect();
+
+
+
 let MinHeap = function() {
 	
 	let heap = [null];
+	addNode("Root", svgSize.width / 2, 70, "white", "root")
 	
 	this.insert = function(num) {
 		heap.push(num);
@@ -131,4 +151,65 @@ let MaxHeap = function() {
 	};
 
 };
+
+
+let myMinHeap = new MinHeap();
+
+
+
+
+function addNode(value, x, y, colour, root) {
+
+
+	let svg = document.querySelector("#heapSVG");
+
+	const svgns = "http://www.w3.org/2000/svg";
+
+	let newCircle = document.createElementNS(svgns, "circle");
+
+	newCircle.setAttribute("cy", y);
+	newCircle.setAttribute("cx", x);
+	newCircle.setAttribute("r", "20");
+	newCircle.setAttribute("stroke", colour);
+	newCircle.setAttribute("id", value);
+	newCircle.setAttribute("class", root);
+
+
+	svg.appendChild(newCircle);
+
+	let newText = document.createElementNS(svgns, "text");
+	newText.setAttributeNS(null, "y", y);
+	newText.setAttributeNS(null, "x", x);
+	newText.setAttributeNS(null, "stroke", "white");
+	newText.setAttributeNS(null, "text-anchor", "middle");
+	newText.setAttributeNS(null, "id", value);
+	newText.setAttributeNS(null, "class", root);
+	var textNode = document.createTextNode(value);
+	newText.appendChild(textNode);
+
+	svg.appendChild(newText);
+
+}
+
+function addLine(fromX, fromY, toX, toY, root, value) {
+
+	let svg = document.querySelector("#trieSVG");
+
+	const svgns = "http://www.w3.org/2000/svg";
+	let newLine = document.createElementNS(svgns, "line");
+	newLine.setAttribute("x1", fromX);
+	newLine.setAttribute("y1", fromY);
+	newLine.setAttribute("x2", toX);
+	newLine.setAttribute("y2", toY);
+	newLine.setAttribute("stroke-width", 1);
+	newLine.setAttribute("stroke", "white");
+	newLine.setAttribute("z-index", "-1");
+	newLine.setAttribute("class", root);
+	newLine.setAttribute("id", value);
+
+	svg.appendChild(newLine);
+
+}
+
+
 
