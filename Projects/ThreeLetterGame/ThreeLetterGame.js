e1715c7d60f9c8b078fc21e99acc69ab7b2746da
Jamesprunty <121761095+Jamesprunty@users.noreply.github.com>
2023-06-09 13:18:16 +0100

    Update ThreeLetterGame.js

--- Projects/ThreeLetterGame/ThreeLetterGame.js
+++ Projects/ThreeLetterGame/ThreeLetterGame.js
@@ -5,7 +5,7 @@ let lettersArr = generate(3);
 
 input.addEventListener("keypress", function(e){
     if(e.key == "Enter"){
-        console.log("ENTER");
+        wordChecker(lettersArr, input.value);
     }
 })
 
@@ -63,8 +63,78 @@ function randomLetters(amount){
 
 }
 
-function wordChecker(lettersArr, input){
+function wordChecker(letters, input){
 
+   
+
+        
+    // split into words
+    let words = input.split(" ");
+    console.log(words);
+    let wordAmount = words.length;
+    console.log(wordAmount);
+
+     //If the words are 3 letters return
+
+     words.forEach(element => {
+        console.log(element);
+        if(element.length <= 2){
+            console.log("too short");
+            return "too Short";
+        }
+     });
+
+
+    //Check if the words are words, if they are not, then return false
+
+    dictionaryCheck(words);
+
+
+
+
+    let check = [];
+    checkIdx = 0;
+
+    for (let j = 0; j < letters.length; j++) {
+        check.push(0);
+        
+    }
+    console.log(check);
+
+console.log(letters);
+console.log(input);
+
+//Check it contains the letters and the letters are in the correct order, if they don't return true
+
+for (let i = 0; i < input.length; i++) {
+    
+    if(input[i] == letters[checkIdx]){
+        check[checkIdx] = 1;
+        checkIdx += 1;
+    };
+}
+
+console.log(check);
+
+
+
+//calculate score and add to previous lise, then generate a new set of letters. 
+
+
+}
+
+function dictionaryCheck(words){
+
+    words.forEach(element => {
+        
+        let api = "https://api.dictionaryapi.dev/api/v2/entries/en/" + element;
+        console.log(api);
+
+
+
+
+
+    });
 
 
 
