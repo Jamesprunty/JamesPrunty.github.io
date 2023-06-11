c01e5e5e00f1a97af5f970a90a5f5d054638e448
Jamesprunty <james107@hotmail.com>
2023-06-11 13:38:03 +0100

    TLG

--- Projects/ThreeLetterGame/ThreeLetterGame.js
+++ Projects/ThreeLetterGame/ThreeLetterGame.js
@@ -3,6 +3,27 @@ const current = document.querySelector("#current");
 const input = document.querySelector("#input");
 const previousDiv = document.querySelector("#previousDiv");
 const timerDiv = document.querySelector("#timerDiv");
+const popupContent = document.querySelector("#popupContent");
+const highScoreCount = 5;
+const highScores = 'highScores';
+let scoresArray = [];
+
+
+
+
+window.addEventListener("load", function () {
+    setTimeout(
+        function open(event) {
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
 
 let number = 3;
 let lettersArr = generate(number);
@@ -27,12 +48,12 @@ input.addEventListener("keypress", function (e) {
         success = true;
         inputLength = true;
 
-            console.log(wordChecker(lettersArr, input.value));
+        wordChecker(lettersArr, input.value);
 
-           
 
-        
-        
+
+
+
     }
 })
 
@@ -83,7 +104,7 @@ function randomLetters(amount) {
 
 function wordChecker(letters, input) {
 
-    
+
 
     let trueArray = [];
 
@@ -96,20 +117,20 @@ function wordChecker(letters, input) {
 
     words.forEach(element => {
         if (element.length <= 2) {
-            longEnough = false;  
+            longEnough = false;
         }
     });
 
     let firstLetters = words[0].split("");
 
-    if(firstLetters[0] == undefined){
+    if (firstLetters[0] == undefined) {
         letterFirst = false;
-    }else{
+    } else {
         letterFirst = true;
     }
 
 
-    if(input.length < 4){
+    if (input.length < 4) {
         inputLength = false;
     }
 
@@ -117,24 +138,24 @@ function wordChecker(letters, input) {
     //Check if the words are words, if they are not, then return false
 
     let wordCheckArray = [];
-   
-words.forEach(element => {
-    dictionaryCheck(element).then((responseData) => {
-      wordCheckArray.push(responseData);});
+
+    words.forEach(element => {
+        dictionaryCheck(element).then((responseData) => {
+            wordCheckArray.push(responseData);
+        });
 
 
-});
-
-setTimeout(() => {
-
-    console.log(wordCheckArray);
-    wordCheckArray.forEach(element => {
-        if(element == false){
-            isWord = false;
-        }
     });
-    
-}, 1000);
+
+    setTimeout(() => {
+
+        wordCheckArray.forEach(element => {
+            if (element == false) {
+                isWord = false;
+            }
+        });
+
+    }, 1000);
 
 
 
@@ -155,142 +176,135 @@ setTimeout(() => {
             checkIdx += 1;
         };
     }
- 
+
 
     check.forEach(element => {
-        if(parseInt(element) == 0){
+        if (parseInt(element) == 0) {
             letterCheck = false;
         }
     });
 
 
-setTimeout(() => {
+    setTimeout(() => {
 
-    let successCheck = [];
-    let successCheckData = [];
-    successCheck.push(longEnough);
-    successCheckData.push("Long Enough: " + longEnough);
-    successCheck.push(letterFirst);
-    successCheckData.push("Letter First: " + letterFirst);
-    successCheck.push(letterCheck);
-    successCheckData.push("Letter Check: " + letterCheck);
-    successCheck.push(inputLength);
-    successCheckData.push("Input Data: " + inputLength);
-    successCheck.push(isWord);
-    successCheckData.push("Is Word: " + isWord);
-
-    console.log(isWord);
+        let successCheck = [];
+        let successCheckData = [];
+        successCheck.push(longEnough);
+        successCheckData.push("Long Enough: " + longEnough);
+        successCheck.push(letterFirst);
+        successCheckData.push("Letter First: " + letterFirst);
+        successCheck.push(letterCheck);
+        successCheckData.push("Letter Check: " + letterCheck);
+        successCheck.push(inputLength);
+        successCheckData.push("Input Data: " + inputLength);
+        successCheck.push(isWord);
+        successCheckData.push("Is Word: " + isWord);
 
 
 
-    successCheck.forEach(element => {
-        if(element == false){
-            success = false;
-        }
-    });
 
-    let upperLetters = [];
-
-    for (let i = 0; i < letters.length; i++) {
-        upperLetters.push(letters[i].toLocaleUpperCase()) ;
-
-    }
-
-
-    console.log(successCheck);
-
-    if(success == true){
-        
-        //Process points
-
-        let points = 0;
-
-        console.log(input);
-
-        let inputData = input.split("");
-
-        inputData.forEach(element => {
-            points++;
-            if(element == " "){
-                points += 5;
+        successCheck.forEach(element => {
+            if (element == false) {
+                success = false;
             }
         });
 
-       
-        input.value = "";
+        let upperLetters = [];
+
+        for (let i = 0; i < letters.length; i++) {
+            upperLetters.push(letters[i].toLocaleUpperCase());
+
+        }
+
+
+
+        if (success == true) {
+
+            //Process points
+
+            let points = 0;
+
+
+            let inputData = input.split("");
+
+            inputData.forEach(element => {
+                points++;
+                if (element == " ") {
+                    points += 5;
+                }
+            });
+
+
+            input.value = "";
 
 
 
 
 
-        previousArray.push([upperLetters, words, points - 4]);
-        console.log(previousArray);
+            previousArray.push([upperLetters, words, points - 4]);
 
-        let dataFinish = ""
+            let dataFinish = ""
 
-        previousArray.forEach(element => {
-            console.log(element);
+            previousArray.forEach(element => {
 
-            let data = 
-            `<div class="prevContainer">
+                let data =
+                    `<div class="prevContainer">
             <div class="heading"><p class="lettersContent">${element[0]}</p></div>
             <div class="heading"><p class="wordsContent">${element[1]}</p></div>
             <div class="heading"><p class="wordsContent">${element[2]}</p></div>
             </div>`
 
-        dataFinish += data;
-        let total = 0;
-
-       
-        
-
-        });
-
-
-        console.log(previousDiv);
-        previousDiv.innerHTML = dataFinish;
+                dataFinish += data;
+                let total = 0;
 
 
 
 
-        if(previousArray.length < 6){
-            
-            reset(number);
+            });
 
-        }else{
-           
-            endGame(previousArray);
-            
+
+            previousDiv.innerHTML = dataFinish;
+
+
+
+
+            if (previousArray.length < 2) {
+
+                reset(number);
+
+            } else {
+
+                endGame(previousArray);
+
+            }
+
+
+
+        } else {
+
+            input.value = "";
+
+
+            //return that there was a problem
+
+
+            if (successCheck[0] == false) {
+                alert("The word must be longer.");
+            } else if (successCheck[1] == false) {
+                alert("The first character is not a letter.");
+            } else if (successCheck[2] == false) {
+                alert("The words do not contain the letters or are not in the right order.");
+            } else if (successCheck[3] == false) {
+                alert("The input must be at least 4 letters in length.");
+            } else if (successCheck[4] == false) {
+                alert("One of the words is not a word.");
+            }
+
+
         }
 
-        
 
-    }else{
-
-        input.value = "";
-        
-
-        //return that there was a problem
-
-        console.log(successCheckData[0]);
-
-        if(successCheck[0] == false){
-            alert("The word must be longer.");
-        }else if(successCheck[1] == false){
-            alert("The first character is not a letter.");
-        }else if(successCheck[2] == false){
-            alert("The words do not contain the letters or are not in the right order.");
-        }else if(successCheck[3] == false){
-            alert("The input must be at least 4 letters in length.");
-        }else if(successCheck[4] == false){
-            alert("One of the words is not a word.");
-        }
-
-
-    }
-
-    
-}, 1000);
+    }, 1000);
 
 
     //calculate score and add to previous lise, then generate a new set of letters. 
@@ -305,58 +319,125 @@ async function dictionaryCheck(word) {
 
 
     return await fetch(url)
-    .then((response) => response.json())
-    .then((responseData) => {
-      console.log(responseData);
+        .then((response) => response.json())
+        .then((responseData) => {
 
-      if(responseData.message){
-        return false;
-      }else{
-        return true;
-      }
-    })
-    .catch(error => console.warn(error));
-  }
+            if (responseData.message) {
+                return false;
+            } else {
+                return true;
+            }
+        })
+        .catch(error => console.warn(error));
+}
 
-  function reset(number){
+function reset(number) {
 
     input.value = "";
 
-        longEnough = true;
-        letterFirst = true;
-        isWord = true;
-        letterCheck = true;
-        success = true;
-        inputLength = true;
+    longEnough = true;
+    letterFirst = true;
+    isWord = true;
+    letterCheck = true;
+    success = true;
+    inputLength = true;
 
-        lettersArr = generate(number);
+    lettersArr = generate(number);
 
 
-  }
+}
 
-  function endGame(){
+function endGame() {
 
     let total = 0;
 
-    //SAVE TOP SCORES TO LOCALSTORAGE
-
 
     for (let i = 1; i < previousArray.length; i++) {
-        console.log(previousArray);
         total += previousArray[i][2];
-        
-        
     }
 
-    timerDiv.innerHTML = `<p id="score">Total: ${total}</p>`
+
+
+    if(localStorage.getItem("highScores0")){
+
+        let tempScores = [];
+        let inserted = false;
+
+     for (let j = 0; j < 6; j++) {
+        if(localStorage.getItem("highScores" + j)){
+        scoresArray.push(localStorage.getItem("highScores" + j));
+        }
+     }
+
+     console.log(scoresArray);
+
+
+
+     for (let i = 0; i < 6; i++) {
+
+        console.log(scoresArray[i]);
+
+        if(total <= scoresArray[i] && inserted == false){
+            tempScores.push(total);
+            inserted = true;
+            tempScores.push(scoresArray[i]);
+        }else if(scoresArray[i + 1] == undefined && inserted == false){
+
+            tempScores.push(scoresArray[i]);
+            console.log("TESTNIG");
+            tempScores.push(total);
+            inserted = true;
+            
+
+        }else{
+            tempScores.push(scoresArray[i]);
+        }
+     }
+
+     console.log(tempScores);
+
+     for (let k = 0; k < 6; k++) {
+        localStorage.setItem("highScores" + k, tempScores[k]);
+        
+     }
+
+     console.log(scoresArray);
+
+
+    }else{
+       
+        localStorage.setItem("highScores0", total)
+
+    }
+
+    let scores = previousDiv.innerHTML;
+
+    popupContent.innerHTML = `
+        <h1>Results<h1><br>
+        <h2> Your score was: ${total}</h2><br><br>
+        <h3>Words used</h3>
+        ${scores};
+        <h3>Previous Results</h3>
+        <div id="highScoresTable">
+        <p>1 - ${localStorage.getItem("highScores0")}</p>
+        <p>2 - ${localStorage.getItem("highScores1")}</p>
+        <p>3 - ${localStorage.getItem("highScores2")}</p>
+        <p>4 - ${localStorage.getItem("highScores3")}</p>
+        <p>5 - ${localStorage.getItem("highScores4")}</p>
+        <div id="previousResults"></div>
+        <div id="reset"><input type="button" value="reset" id="resetBtn"></input></div>`
+
+    //timerDiv.innerHTML = `<p id="score">Total: ${total}</p>`
     input.value = "";
+    document.querySelector(".popup").style.display = "block";
 
-  }
+}
 
 
-  //SETUP CONTROLS 
+  //SETUP CONTROLS
   //Setup Start
-  
+  //Setup
+
 
 
 
